6fde8deade1f43160bf875ccd6c627791c40ad5a
Liupeter01 <Liupeter012001@gmail.com>
2023-09-20 10:58:15 +1000

    1.MODIFTY: modifty client socket generate

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,34 +1,14 @@
 #include"HelloClient.h"
 
-constexpr unsigned int g_ClientNumber(1000);
-constexpr unsigned int g_ThreadNumber(4);
-
 int main() 
 {
-          HelloClient* clientPool[g_ClientNumber];
-          std::thread th_send[g_ThreadNumber];
-          for (int i = 0; i < g_ThreadNumber; ++i) 
-          {
-                    th_send[i] = std::thread([&](HelloClient *clientArray[g_ClientNumber], int id) {
-                              std::string clientName = "client-loopback404" + id;
-                              std::string clientPass = "1234567abc" + id;
-                              _LoginData loginData(clientName, clientPass);
-                              for (int i = id * (g_ClientNumber / g_ThreadNumber); i < (id+1)*(g_ClientNumber / g_ThreadNumber); ++i) {
-                                        clientPool[i] = new HelloClient;
-                                        clientPool[i]->connectServer(inet_addr("127.0.0.1"), 4567);
-                              }
-                              while (true){
-                                        for (int i = 0; i < g_ClientNumber; ++i) {
-                                                  clientPool[i]->sendDataToServer(clientPool[i]->getClientSocket(), &loginData, sizeof(loginData));
-                                        }
-                              }
-                              for (int i = 0; i < g_ClientNumber; ++i) {
-                                        delete clientPool[i];
-                              }
-                    }, clientPool,i);
-          }
-          for (int i = 0; i < g_ThreadNumber; ++i) {
-                    th_send[i].join();
-          }
+          /*Create Client Socket*/
+          HelloClient client;
+
+          /*Connect To Server IP Address = 127.0.0.1 ; Port = 4567*/
+          client.connectServer(inet_addr("127.0.0.1"), 4567);
+
+          /*Start Client Basic Logic Function To Handle Network Request*/
+          client.clientMainFunction();
           return 0;
 }
