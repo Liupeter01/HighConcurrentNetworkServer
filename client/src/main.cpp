cda56bb5b940c7e7d79f46c328d0e0518c0495df
Liupeter01 <Liupeter012001@gmail.com>
2023-09-20 10:56:44 +1000

    1.FIX BUGS: there is something wrong with the HelloClient class generation and different threads access to a single socket at a same time and causing client's offline issue

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,34 +1,34 @@
 #include"HelloClient.h"
 
-constexpr unsigned int g_ClientNumber(1000);
-constexpr unsigned int g_ThreadNumber(4);
+constexpr int g_ClientNumber(4000);
+constexpr int g_ThreadNumber(4);
 
-int main() 
+int main()
 {
+          _LoginData loginData("client-loopback404", "1234567abc");
           HelloClient* clientPool[g_ClientNumber];
           std::thread th_send[g_ThreadNumber];
-          for (int i = 0; i < g_ThreadNumber; ++i) 
+
+          for (int i = 0; i < g_ThreadNumber; ++i)
           {
-                    th_send[i] = std::thread([&](HelloClient *clientArray[g_ClientNumber], int id) {
-                              std::string clientName = "client-loopback404" + id;
-                              std::string clientPass = "1234567abc" + id;
-                              _LoginData loginData(clientName, clientPass);
-                              for (int i = id * (g_ClientNumber / g_ThreadNumber); i < (id+1)*(g_ClientNumber / g_ThreadNumber); ++i) {
+                    th_send[i] = std::thread([&](HelloClient* clientArray[g_ClientNumber], int id) {
+                              for (int i = id * (g_ClientNumber / g_ThreadNumber); i < (id + 1) * (g_ClientNumber / g_ThreadNumber); ++i) {
                                         clientPool[i] = new HelloClient;
                                         clientPool[i]->connectServer(inet_addr("127.0.0.1"), 4567);
                               }
-                              while (true){
-                                        for (int i = 0; i < g_ClientNumber; ++i) {
+                              while (true)
+                              {
+                                        for (int i = id * (g_ClientNumber / g_ThreadNumber); i < (id + 1) * (g_ClientNumber / g_ThreadNumber); ++i) {
                                                   clientPool[i]->sendDataToServer(clientPool[i]->getClientSocket(), &loginData, sizeof(loginData));
                                         }
                               }
-                              for (int i = 0; i < g_ClientNumber; ++i) {
-                                        delete clientPool[i];
-                              }
-                    }, clientPool,i);
+                              }, clientPool, i);
           }
           for (int i = 0; i < g_ThreadNumber; ++i) {
                     th_send[i].join();
+          }
+          for (int i = 0; i < g_ClientNumber; ++i) {
+                    delete clientPool[i];
           }
           return 0;
 }
