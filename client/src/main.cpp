2132358cd3ee14cef7c5812d009d4bf4aac39ced
Liupeter01 <Liupeter012001@gmail.com>
2023-09-20 10:57:35 +1000

    1.MODIFTY: modifty Client Socket

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -3,31 +3,12 @@
 int main() 
 {
           /*Create Client Socket*/
-          //HelloClient client;
+          HelloClient client;
 
           /*Connect To Server IP Address = 127.0.0.1 ; Port = 4567*/
-          //client.connectServer(inet_addr("127.0.0.1"), 4567);
+          client.connectServer(inet_addr("127.0.0.1"), 4567);
 
           /*Start Client Basic Logic Function To Handle Network Request*/
-          //client.clientMainFunction();
-
-          /*Create Client Socket*/
-          const int _ClientAmmount = 1000;
-          HelloClient* clientPool[_ClientAmmount];
-          _LoginData loginData("client-loopback404", "1234567abc");
-          for (int i = 0; i < _ClientAmmount; ++i) {
-                    clientPool[i] = new HelloClient;
-                    clientPool[i]->connectServer(inet_addr("127.0.0.1"), 4567);
-          }
-          while (true) 
-          {
-                    for (int i = 0; i < _ClientAmmount; ++i)
-                    {
-                              clientPool[i]->sendDataToServer(clientPool[i]->getClientSocket(), &loginData, sizeof(loginData));
-                    }
-          }
-          for (int i = 0; i < _ClientAmmount; ++i) {
-                    delete clientPool[i] ;
-          }
+          client.clientMainFunction();
           return 0;
 }
